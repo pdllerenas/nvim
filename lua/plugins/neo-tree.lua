48415d3b348edf27ab3a91370d842ce156d8be57
pdllerenas <pdllerenas@gmail.com>
2025-09-06 17:57:11 -0600

    added current working directory change command to neotree
    Signed-off-by: pdllerenas <pdllerenas@gmail.com>

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,14 +3,29 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons",   -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
-    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
   },
-  lazy = false, -- neo-tree will lazily load itself
-  ---@module "neo-tree"
-  ---@type neotree.Config?
+  lazy = false,                      -- neo-tree will lazily load itself
   opts = {
-    -- fill any relevant options here
-  },
+    filesystem = {
+      bind_to_cwd = false, -- don't auto-sync, we'll handle it manually
+      follow_current_file = { enabled = true },
+      window = {
+        mappings = {
+          ["C"] = function(state)
+            local node = state.tree:get_node()
+            if node.type == "directory" then
+              -- Change cwd to the selected directory
+              vim.cmd("cd " .. node.path)
+              require("neo-tree.sources.filesystem").navigate(state, node.path)
+            else
+              -- Open files as usual
+              require("neo-tree.sources.filesystem.commands").open(state)
+            end
+          end,
+        },
+      },
+    },
+  }
 }
